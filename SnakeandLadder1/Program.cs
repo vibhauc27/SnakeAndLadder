2f2c10203dd1753f714c210ecf7e0f5f2b91692e
vibha <vibhauc@gmail.com>
2022-02-14 09:36:55 +0530

    Adding UC1

---
+++ SnakeandLadder1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnakeandLadder1 // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //UC1
+            int position = 0;
+        }
+    }
+
+            
+}
